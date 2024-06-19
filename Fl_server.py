f0975326c595210f2215851f824eb0a4fdabfc30
MinHyeok Lee <vpsxja3268@naver.com>
2024-06-19 17:51:50 +0900

    라우터 기초 학습

--- Fl_server.py
+++ Fl_server.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "hi"
+def test():
+    return 'welcome'
+
+@app.route('/data/')
+def dataset():
+    return 'This is a dataset'
+
+@app.route('/class/1/')
+def class_1():
+    return 'Class No.1'
 
 
-app.run(port=5001)
+
+app.run(port=5001, debug=True)
